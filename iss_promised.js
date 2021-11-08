88ddb85d5b9795673538cdfe0d1867cd7593b024
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-08 02:28:27 +0000

    refactored for promises

---
+++ iss_promised.js
@@ -0,0 +1,15 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://api.freegeoip.app/json/${ip}?apikey=7c8714f0-3df0-11ec-a134-3f1c9da311a1`)
+};
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);             
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
